8b9d91d61ec8d0ca967dd5a862f8dd4b84572270
Shaun Kruger <skruger@fastinfra.com>
2011-12-09 15:34:00 -0700

    Add mnesia/clustering helper functions.

---
+++ src/cluster_mnesia.erl
@@ -0,0 +1,44 @@
+%% Author: skruger
+%% Created: Nov 5, 2011
+%% Description: TODO: Add description to cluster_mnesia
+-module(cluster_mnesia).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([table_copies/0,table_copies/1]).
+-export([get_table_copy_type/2]).
+
+%%
+%% API Functions
+%%
+
+table_copies() ->
+    Tables = mnesia:system_info(tables),
+    lists:flatmap(fun(T) ->
+                    [{T,N,disc_copies} || N <- mnesia:table_info(T,disc_copies)] ++
+                    [{T,N,disc_only_copies} || N <- mnesia:table_info(T,disc_only_copies)] ++
+                    [{T,N,ram_copies} || N <- mnesia:table_info(T,ram_copies)]
+                end,Tables).
+
+table_copies({Node,Table}) ->
+    lists:filter(fun({T,N,_}) when N == Node, T == Table -> true; (_) -> false end,table_copies());
+table_copies(Node) ->
+    lists:filter(fun({_,N,_}) when N == Node -> true; (_) -> false end,table_copies()).
+
+get_table_copy_type(Node,Table) ->
+	case table_copies({Node,Table}) of
+		[{_,_,Type}|_] ->
+			Type;
+		[] ->
+			no_copy
+	end.
+
+%%
+%% Local Functions
+%%
+
